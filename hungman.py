86e2c5a870417b27ae94912c853b61461b1db90e
VadimLevkovich <vadum13531@gmail.com>
2020-04-06 14:12:26 +0300

    Added word list file

--- hungman.py
+++ hungman.py
@@ -4,32 +4,40 @@ import sys
 
 
 def hangman():
-    secret_words = ('python', 'java', 'window', 'sofa', 'chair', 'table', 'pillow', 'bathroom', 'garage', 'notepad')
+    with open('words.txt', 'r') as f:
+        secret_words = f.read().splitlines()
+
     secret_word = random.choice(secret_words)
     guessed_letters = []
     word = ['-' for _ in secret_word]
-    print("".join(word))
     attempts = 8
+
     while attempts > 0:
+        print("".join(word))
         char = input("Input a letter: ")
+
         if not (len(char) == 1 and char in string.ascii_lowercase):
             print("Please, enter one letter in lowercase")
+
         else:
             if char in guessed_letters:
                 print("You already has this letter")
                 continue
+
             guessed_letters.append(char)
+
             if char not in secret_word:
                 attempts -= 1
                 print(f"No such letters in the word, you have {attempts} attempts")
                 continue
+
             for i in range(len(word)):
                 if secret_word[i] == char:
                     word[i] = char
+
             if secret_word == "".join(word):
                 print("You win!")
                 break
-        print("".join(word))
     else:
         print("You are hanged")
 
