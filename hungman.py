245ddc93171b0d5bbd54447110f6508c05f08802
VadimLevkovich <vadum13531@gmail.com>
2020-04-10 23:30:21 +0300

    Minor fixes

--- hungman.py
+++ hungman.py
@@ -1,6 +1,4 @@
 import random
-import string
-import sys
 
 
 def hangman():
@@ -14,40 +12,41 @@ def hangman():
 
     while attempts > 0:
         print("".join(word))
-        char = input("Input a letter: ")
+        char = input('Input a letter: ').casefold()
 
-        if not (len(char) == 1 and char in string.ascii_lowercase):
-            print("Please, enter one letter in lowercase")
+        if len(char) != 1:
+            print('Please, enter one letter')
 
         else:
             if char in guessed_letters:
-                print("You already has this letter")
+                print('You already has this letter')
                 continue
 
             guessed_letters.append(char)
 
             if char not in secret_word:
                 attempts -= 1
-                print(f"No such letters in the word, you have {attempts} attempts")
+                print(f'No such letters in the word, you have {attempts} attempts')
                 continue
 
             for i in range(len(word)):
                 if secret_word[i] == char:
                     word[i] = char
 
-            if secret_word == "".join(word):
-                print("You win!")
+            if secret_word == ''.join(word):
+                print(f'You win!')
                 break
     else:
-        print("You are hanged")
+        print('You are hanged')
+    print(f'The word is "{secret_word}"')
 
 
 if __name__ == '__main__':
     while True:
-        choice = input("Type 'play' to play the game, 'exit' to quit: ")
+        choice = input('Type "play" to play the game, "exit" to quit: ').casefold()
         if choice == 'play':
             hangman()
         elif choice == 'exit':
-            sys.exit()
+            exit()
         else:
-            print("Incorrect choice, please, try again!")
+            print('Incorrect choice, please, try again!')
